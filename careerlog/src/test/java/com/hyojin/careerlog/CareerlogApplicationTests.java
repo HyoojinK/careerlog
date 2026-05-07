76bb860378bb803ad9fa74ff335d7f4ea4dd522f
김효진 <kyg0328@naver.com>
2026-05-07 12:47:47 +0900

    Spring Boot 프로젝트 초기 설정

---
+++ careerlog/src/test/java/com/hyojin/careerlog/CareerlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyojin.careerlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
