ef22b5d26585d989d23f8dece246a53bb5b7cc60
김효진 <kyg0328@naver.com>
2026-05-10 01:36:57 +0900

    페이지 구조 설정

---
+++ src/main/java/com/careerlog/controller/PageController.java
@@ -0,0 +1,23 @@
+package com.careerlog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class PageController {
+	@GetMapping("/")
+	public String home()
+	{
+		return "redirect:/dashboard";
+	}
+
+	@GetMapping("/{view}")
+	public String moveView(
+		@PathVariable String view
+	)
+	{
+		return view;
+	}
+	
+}
---
+++ src/main/resources/static/js/common/layout.js
@@ -0,0 +1,26 @@
+$(function(){
+	onloadBtnEvent();
+});
+
+function onloadBtnEvent() {
+	$('#userInfo').on('click', function(){
+		var $this = $(this);
+		var $ul = $this.next('ul');
+
+		if($ul.is(':visible'))
+		{
+			$ul.slideUp(200);
+			$this.removeClass('open');
+		}
+		else
+		{
+			$ul.slideDown(200);
+			$this.addClass('open');
+		}
+	});
+	
+	$('.menu_wrap li').on('click', function()
+	{
+		location.href = $(this).data('url');
+	});
+}
