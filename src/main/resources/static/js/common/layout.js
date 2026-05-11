a7f20e91131835c4628a6d8e30e086c2126d7ac0
김효진 <kyg0328@naver.com>
2026-05-12 01:21:18 +0900

    add motivation quote widget

--- src/main/resources/static/js/common/layout.js
+++ src/main/resources/static/js/common/layout.js
@@ -1,6 +1,45 @@
-$(function(){
+$(function() {
+	getQuote();
 	onloadBtnEvent();
 });
+
+function getQuote(){
+	var quoteData = sessionStorage.getItem('quoteData');
+
+	if(quoteData)
+	{
+		quoteData = JSON.parse(quoteData);
+		renderQuote(quoteData);
+		return;
+	}
+
+	$.ajax(
+	{
+		url: 'https://korean-advice-open-api.vercel.app/api/advice',
+		type: 'GET',
+		dataType: 'json',
+		success: function(result)
+		{
+			sessionStorage.setItem('quoteData',JSON.stringify(result));
+			renderQuote(result);
+		},
+		error: function()
+		{
+			renderQuote(
+			{
+				message: '오늘의 작은 노력이 내일의 나를 만듭니다.',
+				author: 'CareerLog',
+				authorProfile: '오늘의 응원'
+			});
+		}
+	});
+}
+
+function renderQuote(data) {
+	$('#quoteText').text('“' + data.message + '”');
+	$('#quoteAuthor').text(data.author);
+	$('#quoteAuthorProfile').text(data.authorProfile);
+}
 
 function onloadBtnEvent() {
 	$('#userInfo').on('click', function(){
@@ -24,3 +63,4 @@ function onloadBtnEvent() {
 		location.href = "/page" + $(this).data('url');
 	});
 }
+
